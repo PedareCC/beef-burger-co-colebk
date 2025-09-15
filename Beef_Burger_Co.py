09c478f16b385d5c97c349976ea976cccfd874bb
colebk <165856511+colebk@users.noreply.github.com>
2025-09-15 05:27:54 +0000

    forgot to commit

--- Beef_Burger_Co.py
+++ Beef_Burger_Co.py
@@ -6,8 +6,13 @@ beef_burgers = {
     'The Clogger': 6.70
 }
 
-total_order = []          
-total_order_prices = []   
+def prettyprint(dic):
+    return ', '.join([f'{key} {dic[key]}x' for key in dic.keys()])
+
+total_order = {
+    
+}          
+total_order_prices = 0  
 
 def acurrency(x):
     return f"${x:.2f}"
@@ -19,12 +24,34 @@ def menu():
     print(f"3. The Clogger for {acurrency(beef_burgers['The Clogger'])}")
     print('4. Go to checkout.')
 
+def addbugr(choice):
+    global total_order, total_order_prices
+    if choice not in total_order:
+            total_order[choice] = 1
+    else:
+            total_order[choice] += 1
+    total_order_prices = total_order_prices + 4.99
+    total_order_prices = round(total_order_prices, 2)
+    print(f"understood your full order at the moment is {prettyprint(total_order)} ")
+    print(f"your order price at the moment is {total_order_prices} ")
+    customer_order()    
+     
 
 def customer_order():
-        order = input('Select what you would like to order: ')
+        global total_order, total_order_prices
+        order = int(input('Select what you would like to order: '))
   
         if order == 1:
-            print("sdfg")
+            addbugr('Cheeseburger')
+        if order == 2:
+            addbugr('Double Cheeseburger')
+        if order == 3:
+            addbugr('The Clogger')
+        else:
+             print("\nthats not an option\n")
+             menu()
+             print(f"your order currently contains{total_order}")
+             customer_order()
 
 def check_out():
     print('\n--- Your Order Summary ---')
